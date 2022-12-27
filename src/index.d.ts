02bc3180624dad855aa2c7b0b7e52c698061d897
Tomer Aberbach <tomeraberbach@gmail.com>
2022-12-27 00:44:21 -0500

    docs: add docs to the function itself

--- src/index.d.ts
+++ src/index.d.ts
@@ -14,6 +14,17 @@
  * limitations under the License.
  */
 
+/**
+ * Returns a promise that resolves with an array of results when all of the
+ * given promises resolve, or that rejects with the first rejecting promise in
+ * the given iterable in iteration order.
+ *
+ * The described behavior ensures that for the same iteration order of resolving
+ * and rejecting promises, the same promise would be returned regardless of how
+ * much time it takes each promise to resolve.
+ *
+ * @see https://github.com/TomerAberbach/dpa#huh-what-why
+ */
 declare const dpa: typeof Promise.all
 
 export default dpa
